d7a40afa48c7c7bddb94e10890b0177d53c88550
chn650 <chn650@users.noreply.github.com>
2014-07-09 14:05:00 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -24,8 +24,7 @@ lines(data$datetime, data$Sub_metering_2, col="red")
 lines(data$datetime, data$Sub_metering_3, col = "blue")
 
 #include legend without border 
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, 
-col=c("black", "red", "blue"), bty = "n", cex=0.9)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"), bty = "n", cex=0.9)
 
 
 #fourth plot: datetime vs. Global_reactive_power
